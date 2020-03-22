71c321582bf99ea627e66f8a9db37d3364129c34
Ira <ira.d.sanchez@gmail.com>
2020-03-22 16:20:27 -0700

    Add Notes
    - These are the notes from our review on Sunday where we discussed the fetch and post issues you were having.

--- smurfs/src/actions/smurfsActions.js
+++ smurfs/src/actions/smurfsActions.js
@@ -1,28 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const GET_SMURF = 'GET_SMURF';
-export const POST_SMURF = 'POST_SMURF';
+export const GET_SMURF = "GET_SMURF";
+export const POST_SMURF = "POST_SMURF";
 
+//needs 3 dispatches
 export const getSmurf = () => dispatch => {
-    axios.get('http://localhost:3333/smurfs')
+  axios
+    .get("http://localhost:3333/smurfs")
     .then(res => {
-        //console.log(res.data, "from Actions")
+      //console.log(res.data, "from Actions")
 
-        dispatch({type: 'GET_SMURF', payload: res.data})
+      dispatch({ type: "GET_SMURF", payload: res.data }); //GET_SMURF
     })
     .catch(err => {
-        console.log('something went wrong', err)
-    })
-}
+      console.log("something went wrong", err);
+    });
+};
 
-export const postSmurf = (newSmurf) => dispatch => {
-    //const body = JSON.stringify({name, age, height})
-    axios.post('http://localhost/3333/smurfs', newSmurf)
+//needs 3 dispatches
+export const postSmurf = newSmurf => dispatch => {
+  //const body = JSON.stringify({name, age, height})
+
+  //dispatch to say you're beginning a request
+  axios
+    .post("http://localhost/3333/smurfs", newSmurf) //item is added to the server here
     .then(res => {
-        console.log(res, "from postSmurfs");
-        dispatch({type: POST_SMURF, payload: res.data})
+      console.log(res, "from postSmurfs");
+      //dispatch to say your request went well and you got a response
+      dispatch({ type: POST_SMURF, payload: res.data });
     })
     .catch(err => {
-        console.log("Some Went Wrong, Error: ", err)
-    })
-}
+      //dispatch to say your request failed with an error
+      console.log("Some Went Wrong, Error: ", err);
+    });
+};
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,45 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { connect } from 'react-redux';
-import { getSmurf } from '../actions/smurfsActions';
-import SmurfsForm from './SmurfsForm';
-import SmurfsList from './SmurfsList';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getSmurf } from "../actions/smurfsActions";
+import SmurfsForm from "./SmurfsForm";
+import SmurfsList from "./SmurfsList";
 
 import "./App.css";
 
+const App = props => {
+  //useState not needed because getSmurf triggers the reducer. State lives there.
 
+  useEffect(() => {
+    props.getSmurf();
+  }, []);
 
-const App = (props) => {
-  const [state, setState ] = useState()
-
-  console.log(props, "@@@@@ from App.js")
-
-  
-    useEffect(() => {
-     props.getSmurf()
-   }, [])
- 
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        {/* <div>Start inside of your `src/index.js` file!</div>
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      {/* <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div> */}
-        <SmurfsForm />
-        <SmurfsList smurfs={props.smurfs} getSmurf={props.getSmurf}/>
-        
-        {/* <button onClick={() => props.getSmurf()}>Call Smurfs</button> */}
-      
+      <SmurfsForm />
+      <SmurfsList smurfs={props.smurfs} getSmurf={props.getSmurf} />
+
+      {/* <button onClick={() => props.getSmurf()}>Call Smurfs</button> */}
+
       {/* {props.smurfs.map((item, i) => (
         <h1 key={i}>{item.name}</h1>
       ))} */}
-
-      </div>
-    );
-  
-}
+    </div>
+  );
+};
 export const mapStateToProps = state => {
   return {
-      smurfs: state.smurfs
-  }
-}
-export default connect(mapStateToProps, {getSmurf})(App);
+    smurfs: state.smurfs
+  };
+};
+export default connect(mapStateToProps, { getSmurf })(App);
